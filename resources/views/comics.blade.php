46214cb8b985279dd0ac568551ba2ae20cdca561
GiuseppeMandriani <giuseppe.mandriani@gmail.com>
2021-06-08 17:41:14 +0200

    add config

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -6,8 +6,22 @@
 
 @section('content')
 {{-- COMICS MAIN --}}
-<section>
-    LIST COMICS HERE
+<section class="comics">
+    <h2 class="title-comics">Chose your Comics</h2>
+    <section class="container">
+        <div class="cards">
+            @foreach ($cards as $card )
+                <div class="card">
+                    <img src="{{ $card['image'] }}" alt="">
+                    <h4 class="card-title">{{ $card['title'] }}</h4>
+                    <h4 class="card-price">{{ $card['price'] }} €</h4>
+                    
+                </div>
+                
+            @endforeach
+        </div>
+    
+    </section>
 </section>
 
 @endsection
