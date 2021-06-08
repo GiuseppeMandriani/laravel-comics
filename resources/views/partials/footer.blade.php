a53b8ce8c0caf7e7c1ea6f726a41d09e18819c92
GiuseppeMandriani <giuseppe.mandriani@gmail.com>
2021-06-08 16:33:40 +0200

    header footer ok

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,11 +1,28 @@
 <footer>
 <div class="foot-content">
+<div class="foot-up"></div>
     <div class="foot-flex container">
-        <div class="foot left">
-            tasto
+        <div class="left">
+            <button class="btn foot"><a href="">sign-up now!</a></button>
         </div>
-        <div class="foot right">
-            social buttons
+        <div class="right">
+            <h3 class="footer-follow">follow us</h3>
+            <ul class="social-buttons">
+                <li>
+                    <a href=""><img src="{{ asset('images/footer-facebook.png') }}" alt="Logo FB"></a>
+                </li>
+                <li>
+                    <a href=""><img src="{{ asset('images/footer-twitter.png') }}" alt="Logo Twitter"></a>
+                </li>
+                <li>
+                    <a href=""><img src="{{ asset('images/footer-youtube.png') }}" alt="Logo you tube"></a>
+                </li>
+                <li>
+                    <a href=""><img src="{{ asset('images/footer-pinterest.png') }}" alt="Logo Pinterest"></a>
+                </li>
+                <li>
+                <a href=""><img src="{{ asset('images/footer-periscope.png') }}" alt="Logo Periscope"></a></li>
+            </ul>
         </div>
     </div>
 
