f5a82da9f312db893be9d3982c77f775497ed317
GiuseppeMandriani <giuseppe.mandriani@gmail.com>
2021-06-08 20:33:44 +0200

    add jumbo in home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,10 +8,14 @@
 @section('head-title', 'Home | DC COMICS')      
     
 
-@section('content')
-{{-- SECTION MAIN --}}
-<section>
-    HOME PAGE MAIN HERE
+@section('jumbo-content')
+{{-- SECTION JUMBO --}}
+<section class="jumbotron">
+    <img src="https://www.dccomics.com/sites/default/files/dc-hero_20210607_THOTL1_60be97a3d703e3.16551192.jpg" alt="">
+    <div class="jumbo-content">
+        <h1 class="jumbo-title">the ultimate escape</h1>
+        <h2 class="jumbo-subtitle">The horrors in The nice House on the lake aren't what you expect. They're far, far worse...</h2>
+    </div>
 </section>
 
 @endsection
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -24,6 +24,7 @@
 
         {{-- MAIN --}}
         <main>
+            @yield('jumbo-content')
             @yield('content')
         </main>
 
