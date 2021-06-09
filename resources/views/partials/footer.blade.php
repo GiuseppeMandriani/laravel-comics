faa69985c4484034079d3e6c723c6aa5b8aabefd
GiuseppeMandriani <giuseppe.mandriani@gmail.com>
2021-06-09 09:12:31 +0200

    start component social media buttons

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -7,22 +7,7 @@
         </div>
         <div class="right">
             <h3 class="footer-follow">follow us</h3>
-            <ul class="social-buttons">
-                <li>
-                    <a href=""><img src="{{ asset('images/footer-facebook.png') }}" alt="Logo FB"></a>
-                </li>
-                <li>
-                    <a href=""><img src="{{ asset('images/footer-twitter.png') }}" alt="Logo Twitter"></a>
-                </li>
-                <li>
-                    <a href=""><img src="{{ asset('images/footer-youtube.png') }}" alt="Logo you tube"></a>
-                </li>
-                <li>
-                    <a href=""><img src="{{ asset('images/footer-pinterest.png') }}" alt="Logo Pinterest"></a>
-                </li>
-                <li>
-                <a href=""><img src="{{ asset('images/footer-periscope.png') }}" alt="Logo Periscope"></a></li>
-            </ul>
+            @include('partials.socialmediabuttons')
         </div>
     </div>
 
---
+++ resources/views/partials/socialmediabuttons.blade.php
@@ -0,0 +1,17 @@
+<ul class="social-buttons">
+    <li>
+        <a href=""><img src="{{ asset('images/footer-facebook.png') }}" alt="Logo FB"></a>
+        </li>
+        <li>
+        <a href=""><img src="{{ asset('images/footer-twitter.png') }}" alt="Logo Twitter"></a>
+        </li>
+        <li>
+            <a href=""><img src="{{ asset('images/footer-youtube.png') }}" alt="Logo you tube"></a>
+        </li>
+        <li>
+            <a href=""><img src="{{ asset('images/footer-pinterest.png') }}" alt="Logo Pinterest"></a>
+        </li>
+        <li>
+            <a href=""><img src="{{ asset('images/footer-periscope.png') }}" alt="Logo Periscope"></a>
+        </li>
+</ul>
